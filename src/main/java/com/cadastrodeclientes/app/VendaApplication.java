c8fe4beb2ed27fe61ebd608d2fd097dade9cdeed
Paulo Almeida Morais <115854053+pauloalmeidamorais@users.noreply.github.com>
2022-11-03 12:52:22 -0300

    estrutura inicializada

---
+++ src/main/java/com/cadastrodeclientes/app/VendaApplication.java
@@ -0,0 +1,20 @@
+package com.cadastrodeclientes.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendaApplication {
+
+	@GetMapping("/hello")
+	public String helloWorld(){
+		return "hello world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaApplication.class, args);
+	}
+}
