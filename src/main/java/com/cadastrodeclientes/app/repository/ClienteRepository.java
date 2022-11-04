abaf7537c8ae9af6e3e36d51a2c1a88864ec5982
Paulo Almeida Morais <115854053+pauloalmeidamorais@users.noreply.github.com>
2022-11-04 17:32:09 -0300

    primeiros serviços criados

--- src/main/java/com/cadastrodeclientes/app/repository/ClienteRepository.java
+++ src/main/java/com/cadastrodeclientes/app/repository/ClienteRepository.java
@@ -1,7 +1,11 @@
 package com.cadastrodeclientes.app.repository;
 
+import com.cadastrodeclientes.app.model.Cliente;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ClienteRepository {
+    public void persistir(Cliente cliente){
+        //acessa a base para salvar cliente
+    }
 }
--- src/main/java/com/cadastrodeclientes/app/service/ClienteService.java
+++ src/main/java/com/cadastrodeclientes/app/service/ClienteService.java
@@ -1,12 +1,15 @@
 package com.cadastrodeclientes.app.service;
 
 import com.cadastrodeclientes.app.model.Cliente;
+import com.cadastrodeclientes.app.repository.ClienteRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
     public void salvarCliente(Cliente cliente){
         validarCliente(cliente);
+        ClienteRepository clienteRepository = new ClienteRepository();
+        clienteRepository.persistir(cliente); //salvar cliente
     }
     public void validarCliente(Cliente cliente){
         //aplicarei validações
